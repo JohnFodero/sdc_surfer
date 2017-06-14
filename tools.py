b54b79ade667b460e07bbcaafc30674e0ef95705
John Fodero <jtfodero@gmail.com>
2017-06-14 10:17:35 -0400

    added Udacity model

--- tools.py
+++ tools.py
@@ -1,3 +1,38 @@
+import csv
+import cv2
+import numpy as np
+
 def map_range(x, min_x, max_x, min_y, max_y):
     return ( (x - min_x) * ((max_y - min_y) / (max_x - min_x)) ) + min_y
 
+def import_data(directory):
+
+    raw_X = []
+    raw_y = []
+    data_path = '../inloop'
+    with open(data_path + '/capture_log.csv', 'r') as f:
+        reader = csv.reader(f)
+        iterreader = iter(reader)
+        next(iterreader)
+        for row in iterreader:
+            path = data_path + row[0][1:]
+            img = cv2.imread(path)
+            if img is not None:
+                raw_X.append(img) 
+                raw_y.append(1000*float(row[2]))
+    return np.array(raw_X), np.array(raw_y)
+
+def shuffle_data(X, y):
+    assert len(X) == len(y)
+    p = np.random.permutation(len(X))
+    return X[p], y[p]
+
+def split_data(X, y, train_mark, val_mark):
+    train_mark = int(len(X) * train_mark)
+    val_mark = train_mark + int(len(X) * val_mark)
+
+    train_X, train_y = X[:train_mark], y[:train_mark]
+    val_X, val_y = X[train_mark:val_mark], y[train_mark:val_mark]
+    test_X, test_y = X[val_mark:], y[val_mark:]
+    
+    return train_X, train_y, val_X, val_y, test_X, test_y
